5dd7af9911f2b51549f6def06fac91842de24c15
simran <simran.sidhu2250@gmail.com+simransidhu250@users.noreply.github.com>
2023-05-11 17:51:56 -0700

    insertion of node logic

--- BST.c
+++ BST.c
@@ -1,15 +1,33 @@
 #include <stdlib.h>
+#include <stdio.h>
+
 struct Node{
     int data;
     struct Node* left;
     struct Node* right;
 };
 
-struct Node* root = NULL;
+struct Node* newNode(int val){
+    struct Node* temp =  (struct Node*)malloc(sizeof(struct Node));
+    temp->data = val;
+    temp->left = NULL;
+    temp->right  = NULL;
+    return temp;
+}
 
-void insertNode(){
-    
+struct Node* insertNode(struct Node* root,int val){  
+    if(root == NULL){
+        root = newNode(val);
+    }
+    else if(root->data>val) root->left = insertNode(root->left, val);
+    else if(root->data<val) root->right =insertNode(root->right, val);  
+    return root;
 }
 void main(){
-
+    struct Node* root = NULL;
+    root = insertNode(root, 15);
+    root = insertNode(root, 15);
+    root = insertNode(root, 15);
+    root = insertNode(root, 15);
+    root = insertNode(root, 15);
 }
