9532eb5890acdd0e930b940e7e4ea7ee5bc37c86
simran <simran.sidhu2250@gmail.com+simransidhu250@users.noreply.github.com>
2023-05-11 18:24:15 -0700

    Search funciton implemented

--- BST.c
+++ BST.c
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdbool.h>
+
 
 struct Node{
     int data;
@@ -23,11 +25,28 @@ struct Node* insertNode(struct Node* root,int val){
     else if(root->data<val) root->right =insertNode(root->right, val);  
     return root;
 }
+
+bool  Search(struct Node* root, int val){
+    if(root == NULL) return false;
+    else if(root->data == val) return true;
+    else if(val < root->data) return Search(root->left, val);
+    else return Search(root->right, val);
+
+}
 void main(){
     struct Node* root = NULL;
     root = insertNode(root, 15);
-    root = insertNode(root, 15);
-    root = insertNode(root, 15);
-    root = insertNode(root, 15);
-    root = insertNode(root, 15);
+    root = insertNode(root, 11);
+    root = insertNode(root, 14);
+    root = insertNode(root, 16);
+    root = insertNode(root, 8);
+
+    int n=0;
+    printf("Enter the number you want to search in binary tree: ");
+    scanf("%d", &n);
+    if(Search(root,n)== true)
+        printf("Number found\n");
+    else
+        printf("Number not found\n");
+
 }
