8e489a5b40b755a3964d644af1359ed812ee1c55
simransidhu250 <49374691+simransidhu250@users.noreply.github.com>
2023-05-14 17:54:43 -0700

    Created tree with nodes

--- preorderTraversal.cpp
+++ preorderTraversal.cpp
@@ -7,9 +7,27 @@ struct Node{
     struct Node* right;
 };
 
-void insertNode(){
-    struct Node* root =NULL;
+struct Node* newNode(int data){
+    struct Node* temp = (struct Node*)malloc(sizeof(Node));
+    temp->data = data;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+struct Node* insertNode(struct Node* root, int data){
+    if(root == NULL) {
+        root = newNode(data);
+
+    } 
+    else if(data<=root->data){
+        root->left = insertNode(root->left, data);
+    }
+    else{
+        root->right = insertNode(root->right, data);
+    }
+    return root;
 }
 void main(){
-
+    struct Node* root =NULL;
+    insertNode(root,15);
 }
